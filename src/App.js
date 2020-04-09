224f85117fe3a76b06dcddf3bb68b02e222bde84
Megan Coppins <meganlcoppins@gmail.com>
2020-04-09 15:24:14 -0700

    added links, styled home page

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/index";
 import "materialize-css";
+import About from './components/Carousel/index';
 
 class App extends React.Component {
 
   render(){
     return(
-    <Navbar></Navbar>
+      <div className="container">
+      <Navbar />
+        <div className="layout">
+          <About />
+        </div>
+      </div>
     )
   }
 }
-
-// function App (){
-//   return(
-
-//   )
-// }
 
 export default App;
---
+++ src/components/Carousel/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+function About (){
+    return(
+        <div className="aboutMe">
+            <h1>Hello and Welcome!</h1>
+            <h1>Megan Coppins</h1>
+            <h1>{"<"}Full Stack Developer{"/>"}</h1>
+        </div>
+    )
+}
+
+export default About;
