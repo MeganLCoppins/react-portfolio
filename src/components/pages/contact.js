85fa6b7323cfe9280cf1b3fe0f860b8fc9499e8c
Megan Coppins <meganlcoppins@gmail.com>
2020-05-26 13:48:51 -0700

    added contact form and styling

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -9,23 +9,19 @@ function Contact() {
     <div className="contact">
           <h1>Let's Get In Touch!</h1>
       <div className="backgroundImg">
-        <a href="https://github.com/MeganLCoppins" target="_blank" rel="noopener noreferrer"> <img className="fluid"  
+
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfghwHNFUpa-XQ_YRz70q-_t36Sv9WMNop3yP49P3x-wBgRSA/viewform?embedded=true" width="1200" height="800" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <div className="form" >
+                <a href="https://github.com/MeganLCoppins" target="_blank" rel="noopener noreferrer"> <img className="fluid"  
         src={github}
-        // src="https://pngimg.com/uploads/github/github_PNG83.png" 
-        
         alt="github"></img></a>
         <a href="https://linkedin.com/in/meganlcoppins" target="_blank" rel="noopener noreferrer"> <img className="fluid"  
         src={linkedin}
-        // src="https://cdn3.iconfinder.com/data/icons/free-social-icons/67/linkedin_square_black-512.png" 
         alt="linkedin"></img></a>
         <a href="https://docs.google.com/document/d/1lhn3SBdrLm5w7USTxYUEZpjXkHWvaa75mPgOB6cZwNY/edit?usp=sharing" target="_blank" rel="noopener noreferrer"> <img className="fluid" 
         src={resume}
-        // src="https://image.flaticon.com/icons/png/512/14/14573.png" 
-        
         alt="resume"></img></a>
-
-      <div className="form" >
-        <a href="mailto:meganlcoppins@gmail.com?subject=Requesting%20 Info"><h3>MeganLCoppins@gmail.com</h3></a>
+        
       </div>
       </div>
     </div>
