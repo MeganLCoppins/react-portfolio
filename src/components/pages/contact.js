9211d15037444c63eac8749dd2178bc21aea118d
Megan Coppins <meganlcoppins@gmail.com>
2020-05-26 13:50:19 -0700

    cleaned up code

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -7,22 +7,44 @@ import linkedin from "../../assets/linkedin_icon.webp";
 function Contact() {
   return (
     <div className="contact">
-          <h1>Let's Get In Touch!</h1>
+      <h1>Let's Get In Touch!</h1>
       <div className="backgroundImg">
-
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfghwHNFUpa-XQ_YRz70q-_t36Sv9WMNop3yP49P3x-wBgRSA/viewform?embedded=true" width="1200" height="800" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-      <div className="form" >
-                <a href="https://github.com/MeganLCoppins" target="_blank" rel="noopener noreferrer"> <img className="fluid"  
-        src={github}
-        alt="github"></img></a>
-        <a href="https://linkedin.com/in/meganlcoppins" target="_blank" rel="noopener noreferrer"> <img className="fluid"  
-        src={linkedin}
-        alt="linkedin"></img></a>
-        <a href="https://docs.google.com/document/d/1lhn3SBdrLm5w7USTxYUEZpjXkHWvaa75mPgOB6cZwNY/edit?usp=sharing" target="_blank" rel="noopener noreferrer"> <img className="fluid" 
-        src={resume}
-        alt="resume"></img></a>
-        
-      </div>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfghwHNFUpa-XQ_YRz70q-_t36Sv9WMNop3yP49P3x-wBgRSA/viewform?embedded=true"
+          width="1200"
+          height="800"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Loading…
+        </iframe>
+        <div className="form">
+          <a
+            href="https://github.com/MeganLCoppins"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <img className="fluid" src={github} alt="github"></img>
+          </a>
+          <a
+            href="https://linkedin.com/in/meganlcoppins"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <img className="fluid" src={linkedin} alt="linkedin"></img>
+          </a>
+          <a
+            href="https://docs.google.com/document/d/1lhn3SBdrLm5w7USTxYUEZpjXkHWvaa75mPgOB6cZwNY/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <img className="fluid" src={resume} alt="resume"></img>
+          </a>
+        </div>
       </div>
     </div>
   );
