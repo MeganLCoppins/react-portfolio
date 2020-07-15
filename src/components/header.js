2e2f00c35ba6080e71284286471f2b4affc69a3e
Megan Coppins <meganlcoppins@gmail.com>
2020-07-15 16:16:26 -0700

    added menu button

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header
       style={{
-        padding: ".5rem",
+        padding: ".5%",
         position: "fixed",
         top: "0",
         width: "100%",
@@ -20,12 +20,17 @@ export default function Header() {
           textDecoration: "none",
         }}
       >
-        <h3 style={{ display: `inline`, color: "#008080" }}>Megan Coppins</h3>
+        <h3 style={{ display: `inline`, color: "#008080", float:"right", margin: "0", marginRight: "1.5rem" }}>Megan Coppins</h3>
       </a>
-      <div id="links" style={{ float: "right", marginRight: "2rem" }}>
-        <a href="#about">About</a>
-        <a href="#portfolio">Work</a>
-        <a href="#contact">Contact</a>
+      <div className="dropdown">
+        <div className="divBar bar1"></div>
+        <div className="divBar bar2"></div>
+        <div className="divBar bar3"></div>
+        <div className="dropdown-content">
+          <a href="#about">About</a>
+          <a href="#portfolio">Work</a>
+          <a href="#contact">Contact</a>
+        </div>
       </div>
       <div style={{ float: "clear" }}></div>
     </header>
